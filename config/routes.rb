187d50725a50a7820da149bc3b3c89b229c42a1c
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-02-16 22:28:32 -0500

    modified actions and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :actors 
-  resources :characters, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :characters, only: [:new, :create, :edit, :update, :destroy]
   resources :reviews, only: [:new, :create, :edit, :update, :destroy]
   resources :tvshows
-  resources :users
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
 
   root 'application#home'
 end
