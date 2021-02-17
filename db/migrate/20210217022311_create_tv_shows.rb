d8c4c96d62211d7822879a4f23a8a5673023c3d7
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-02-16 21:25:58 -0500

    generated  models and controllers

---
+++ db/migrate/20210217022311_create_tv_shows.rb
@@ -0,0 +1,13 @@
+class CreateTvShows < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tv_shows do |t|
+      t.string :name
+      t.datetime :start_date
+      t.boolean :currently_airing
+      t.datetime :end_date
+      t.string :network
+
+      t.timestamps
+    end
+  end
+end
