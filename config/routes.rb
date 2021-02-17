f41ba286faa211953f491c8802ad58492e792ed5
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-02-16 21:46:27 -0500

    defined controller actions and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :actors 
+  resources :characters, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :reviews, only: [:new, :create, :edit, :update, :destroy]
+  resources :tvshows
+  resources :users
 end
