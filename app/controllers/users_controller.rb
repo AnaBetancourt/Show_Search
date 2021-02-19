acb6f3f231f30df4ba454ddc1fb56f3e88f33536
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-02-19 00:20:50 -0500

    defined new and create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
     
     def new
+        @user = User.new
     end
 
     def create
+        @user = User.new(user_params)
+
+        if @user.valid?
+            @user.save
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
 
     def show
@@ -18,4 +27,9 @@ class UsersController < ApplicationController
     def destroy
     end
     
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
 end
