c1fd5dfcffbdf0e34ff3046df5d9388f94be17d5
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-02-22 18:30:43 -0500

    began refactoring TvShow model and views

--- app/helpers/tv_shows_helper.rb
+++ app/helpers/tv_shows_helper.rb
@@ -1,2 +1,13 @@
 module TvShowsHelper
+
+    def currently_airing?(show)
+        binding.pry
+
+        if show.currently_airing?
+            "Currently airing"
+        else
+            "Not currently airing"
+        end
+    end
+
 end
