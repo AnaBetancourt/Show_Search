7e3c6ed4a5d8199f1e4b27241e9839d432cf657b
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-02-22 17:49:07 -0500

    wrote out controller and views for tv show model

--- app/controllers/tv_shows_controller.rb
+++ app/controllers/tv_shows_controller.rb
@@ -1,24 +1,44 @@
 class TvShowsController < ApplicationController
     
     def index
+        @shows = TvShow.all
     end
 
     def new
+        @show = TvShow.new
     end
 
     def create
+        @show = TvShow.new(show_params)
+
+        if @show.valid?
+            @show.save
+            redirect_to tv_show_path(@show)
+        else
+            render :new
+        end
     end
 
     def show
+        @show = TvShow.find(params[:id])
     end
 
     def edit
+        @show = TvShow.find(params[:id])
     end
 
     def update
+        binding.pry
     end
 
     def destroy
     end
+    
+    private
+
+    def show_params
+        params.require(:tv_show).permit(:name, :start_date, :end_date, :currently_airing, :synposis, :network)
+    end
+
     
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,11 @@ class UsersController < ApplicationController
     end
 
     def show
+        @user = User.find_by(id: params[:id])
     end
 
     def edit
+        @user = User.find_by(id: params[:id])
     end
 
     def update
