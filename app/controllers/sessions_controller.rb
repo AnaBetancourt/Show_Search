4f36c03cc7cd263a580cf093cb66c637d77b9e0c
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-02-25 14:21:55 -0500

    have skeleton of sessions controller, need to fix and build onto it

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,29 @@
 class SessionsController < ApplicationController
+    skip_before_action :restrict_access, only: [:new, :create]
 
-    # def new
-    #     @user = User.new
-    # end
+    def new
+        @user = User.new
+    end
     
-    # def create
-    #     if @user = User.find_by(name: params[:user][:name])
-    #         session[:user_id] = @user.id
-    #         redirect_to user_path(@user)
-    #     else
-            
-    #     end
-    # end
+    def create
+        # binding.pry
+        if @user = User.find_by(name: session_params[:username])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
     
-    # def destroy
-    #     session.clear
-    #     redirect_to root_path
-    # end
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
+
+    private
+
+    def session_params
+        params.require(:user).permit(:username, :password)
+    end
 
 end
