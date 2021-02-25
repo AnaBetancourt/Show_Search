887348123097863f13838a6c1988b4b77f07c0a4
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-02-25 15:01:58 -0500

    built out sessions controller and login screen

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,28 +2,22 @@ class SessionsController < ApplicationController
     skip_before_action :restrict_access, only: [:new, :create]
 
     def new
-        @user = User.new
     end
     
     def create
-        # binding.pry
-        if @user = User.find_by(name: session_params[:username])
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
         else
-            render :new
+            flash[:message] = "Invalid username or password. Please try again."
+            redirect_to login_path
         end
     end
     
     def destroy
         session.clear
         redirect_to root_path
-    end
-
-    private
-
-    def session_params
-        params.require(:user).permit(:username, :password)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'application#home'
   resources :actors 
   resources :characters, except: [:index]
   resources :reviews, except: [:index, :show]
   resources :tv_shows
   get "/signup", to: "users#new"
-  resources :users, except: [:index, :new]
+  post "/signup", to: "users#create"
+  resources :users, except: [:index, :new, :create]
 
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
-  root 'application#home'
+  
 end
