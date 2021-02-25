411dfa69902db873048584900306fd0f2eaf0296
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-02-25 00:34:29 -0500

    began building out sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,21 @@
 class SessionsController < ApplicationController
 
-    def new
-    end
-
-    def create
-    end
+    # def new
+    #     @user = User.new
+    # end
     
+    # def create
+    #     if @user = User.find_by(name: params[:user][:name])
+    #         session[:user_id] = @user.id
+    #         redirect_to user_path(@user)
+    #     else
+            
+    #     end
+    # end
+    
+    # def destroy
+    #     session.clear
+    #     redirect_to root_path
+    # end
+
 end
