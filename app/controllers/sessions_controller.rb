ce51c34c1888899600d883cab760390563dc6141
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-02-26 22:50:03 -0500

    finished adding omniauth login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,10 +16,15 @@ class SessionsController < ApplicationController
     end
 
     def omniauth
-        user = User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
-            u.name = auth['info']['name']
-        end
+        user = User.create_from_omniauth(auth)
 
+        if user.valid?
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            flash[:message] = user.errors.full_messages.join("")
+            redirect_to root_path
+        end
     end
     
     def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ApplicationRecord
   validates :name, :email, presence: :true
   validates :email, uniqueness: :true
 
+  def self.create_from_omniauth(auth)
+    User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+      u.password = SecureRandom.hex(10)
+    end
+  end
+
 end
