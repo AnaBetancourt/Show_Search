e0edef6319155088b9a3e414a18d78d2b419e2db
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-02-28 15:25:25 -0500

    refactored current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,12 +12,12 @@ class ApplicationController < ActionController::Base
         redirect_to "/" unless logged_in?
     end
 
-    def current_user
-        @current_user ||= User.find_by(id: session[:user_id])
-    end
-
     def logged_in?
         !!session[:user_id]
+    end
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if logged_in?
     end
 
     
