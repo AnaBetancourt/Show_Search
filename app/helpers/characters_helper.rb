ca4c9f1cf09a82b71852728c881ea557dce9cf0b
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-02-28 12:22:42 -0500

    re-factored character form to allow for hidden field when nested

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -1,2 +1,11 @@
 module CharactersHelper
+
+    def display_show_fields(show, f)
+        if show
+            f.hidden_field :tv_show_id
+        else
+            render partial: 'show_select', locals: {f: f}
+        end
+    end
+
 end
