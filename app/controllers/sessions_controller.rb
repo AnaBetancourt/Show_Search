863d70a4e01984faadf3976b064fbfb980a9b98a
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-02-28 15:03:42 -0500

    added styling to flash notices and messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id
             redirect_to user_path(user)
         else
-            flash[:message] = "Invalid email or password. Please try again."
+            flash[:notice] = "Invalid email or password. Please try again."
             redirect_to login_path
         end
     end
@@ -22,7 +22,7 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id
             redirect_to user_path(user)
         else
-            flash[:message] = user.errors.full_messages.join("")
+            flash[:notice] = user.errors.full_messages.join("")
             redirect_to root_path
         end
     end
