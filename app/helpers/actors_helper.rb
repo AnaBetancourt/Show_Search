6fce1e52ca2fcf6ee305168d7dd916c8b1055234
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-02-28 20:04:57 -0500

    refactored actor views and helpers

--- app/helpers/actors_helper.rb
+++ app/helpers/actors_helper.rb
@@ -1,2 +1,17 @@
 module ActorsHelper
+
+    def display_header(show)
+        if show
+            tag.h1("#{show.name}'s Actors")
+        else
+            tag.h1("Actors")
+        end
+    end
+
+    def display_add_character(show)
+        if !show
+            link_to "Create a new actor here", new_actor_path  
+        end
+    end
+
 end
