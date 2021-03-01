5c08d0bb992a82c2fb2a183d950e5c26a7a1c161
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-02-28 21:12:45 -0500

    edited routes and checked all requirement boxes

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,23 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'application#home'
+
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/tv_shows/most_reviewed', to: "tv_shows#reviewed"
+
   resources :users, only: [:edit, :show]
   resources :actors
   resources :characters
-  get '/tv_shows/most_reviewed', to: "tv_shows#reviewed"
+  resources :reviews, except: [:index, :show, :new]
   resources :tv_shows do
     resources :actors, only: [:index]
     resources :characters, only: [:index, :new, :create]
     resources :reviews, only: [:new, :create]
   end
-  resources :reviews, except: [:index, :show, :new]
-
-  get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-
   
 end
