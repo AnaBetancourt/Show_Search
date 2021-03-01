ce32437230c1bfa4bfd9a0b046e7a7473e5f71b7
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-02-28 20:26:03 -0500

    refactored character views and helpers

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -8,4 +8,18 @@ module CharactersHelper
         end
     end
 
+    def display_header(show)
+        if show
+            tag.h1("#{show.name}'s Characters")
+        else
+            tag.h1("Characters")
+        end
+    end
+
+    def display_delete(character)
+        if character.creator == current_user
+            button_to "Delete Character", character_path(@character), :method => :delete
+        end
+    end
+
 end
